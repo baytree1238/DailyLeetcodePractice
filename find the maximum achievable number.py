ff5f07efbfaabcea5dfc427973fca124010068b4
baytree1238 <172351801+baytree1238@users.noreply.github.com>
2025-03-09 09:35:02 +1300

    Add files via upload
    troll problem...

---
+++ find the maximum achievable number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        result = num + 2*t 
+        return result
+'''
+https://leetcode.com/problems/find-the-maximum-achievable-number/
+'''
